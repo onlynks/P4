807583a56806b3db8102627baa9d73154df96e7e
onlynks <nicogar12@gmail.com>
2019-03-22 17:41:27 +0100

    EcritureComptable test ended

--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -1,15 +1,44 @@
 package com.dummy.myerp.model.bean.comptabilite;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.lang3.ObjectUtils;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class EcritureComptableTest {
+	
+	public static EcritureComptable Ecriture1;
+	
+	public static EcritureComptable Ecriture2;
+	
+	public static List<LigneEcritureComptable> listLigneEcriture = new ArrayList<>();
 
-    private LigneEcritureComptable createLigne(Integer pCompteComptableNumero, String pDebit, String pCredit) {
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		Ecriture1 = new EcritureComptable();
+		
+		
+		Ecriture1.setLibelle("Equilibrée");
+		Ecriture1.getListLigneEcriture().add(createLigne(1, "200.50", null));
+		Ecriture1.getListLigneEcriture().add(createLigne(1, "100.50", "33"));
+		Ecriture1.getListLigneEcriture().add(createLigne(2, null, "301"));
+		Ecriture1.getListLigneEcriture().add(createLigne(2, "40", "7"));
+		
+		Ecriture2 = new EcritureComptable();
+		
+		Ecriture2.setLibelle("Non équilibrée");
+		Ecriture2.getListLigneEcriture().add(createLigne(1, "10", null));
+		Ecriture2.getListLigneEcriture().add(createLigne(1, "20", "1"));
+		Ecriture2.getListLigneEcriture().add(createLigne(2, null, "30"));
+		Ecriture2.getListLigneEcriture().add(createLigne(2, "1", "2"));		
+	}
+	
+    private static LigneEcritureComptable createLigne(Integer pCompteComptableNumero, String pDebit, String pCredit) {
         BigDecimal vDebit = pDebit == null ? null : new BigDecimal(pDebit);
         BigDecimal vCredit = pCredit == null ? null : new BigDecimal(pCredit);
         String vLibelle = ObjectUtils.defaultIfNull(vDebit, BigDecimal.ZERO)
@@ -21,24 +50,35 @@ public class EcritureComptableTest {
     }
 
     @Test
-    public void isEquilibree() {
-        EcritureComptable vEcriture;
-        vEcriture = new EcritureComptable();
+    public void testIsEquilibree() {
+       
+        Assert.assertTrue(Ecriture1.toString(), Ecriture1.isEquilibree());
 
-        vEcriture.setLibelle("Equilibrée");
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "200.50", null));
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "100.50", "33"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "301"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, "40", "7"));
-        Assert.assertTrue(vEcriture.toString(), vEcriture.isEquilibree());
-
-        vEcriture.getListLigneEcriture().clear();
-        vEcriture.setLibelle("Non équilibrée");
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "10", null));
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "20", "1"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "30"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, "1", "2"));
-        Assert.assertFalse(vEcriture.toString(), vEcriture.isEquilibree());
+        Assert.assertFalse(Ecriture2.toString(), Ecriture2.isEquilibree());
     }
+    
+    @Test
+    public void testGetTotalDebit(){
+    	BigDecimal value1 = new BigDecimal("341.00");
+    	
+    	Assert.assertEquals(Ecriture1.getTotalDebit(), value1);
+    	
+    	BigDecimal value2 = new BigDecimal("31");
+    	
+    	Assert.assertEquals(Ecriture2.getTotalDebit(), value2);
+    }
+    
+    @Test
+    public void testGetTotalCredit(){
+    	BigDecimal value1 = new BigDecimal("341");
+    	
+    	Assert.assertEquals(Ecriture1.getTotalCredit(), value1);
+    	
+    	BigDecimal value2 = new BigDecimal("33");
+    	
+    	Assert.assertEquals(Ecriture2.getTotalCredit(), value2);
+    }
+    
+    
 
 }
