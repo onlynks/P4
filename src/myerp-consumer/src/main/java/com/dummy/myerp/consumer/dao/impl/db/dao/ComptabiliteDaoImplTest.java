ca1d6fbe180847520b6a0151d3e9631048f79fac
onlynks <nicogar12@gmail.com>
2019-03-29 12:41:18 +0100

    checkpoint

---
+++ src/myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
@@ -0,0 +1,130 @@
+package com.dummy.myerp.consumer.dao.impl.db.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComptabiliteDaoImplTest {
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComptabiliteDaoImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLgetListCompteComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListCompteComptable() {
+		ComptabiliteDaoImpl test = ComptabiliteDaoImpl.getInstance();
+		test.getListCompteComptable();
+	}
+
+	@Test
+	public void testSetSQLgetListJournalComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListJournalComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLgetListEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLgetEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLgetEcritureComptableByRef() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEcritureComptableByRef() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLloadListLigneEcriture() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadListLigneEcriture() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLinsertEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLinsertListLigneEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertListLigneEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLupdateEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLdeleteEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSQLdeleteListLigneEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteListLigneEcritureComptable() {
+		fail("Not yet implemented");
+	}
+
+}
